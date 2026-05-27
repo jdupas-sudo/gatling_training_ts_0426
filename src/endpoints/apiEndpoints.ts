decf2c1a5eeafcfa01481d7fe65da8b7ff35b211
Gatling <gatling@macbookpro.home>
2026-05-27 09:56:55 +0200

    exercice 1-1-1

--- src/endpoints/apiEndpoints.ts
+++ src/endpoints/apiEndpoints.ts
@@ -1,6 +1,12 @@
+import { jmesPath } from "@gatling.io/core";
 import { http } from "@gatling.io/http";
 import { status } from "@gatling.io/http";
 
 export const session = http("Session")
 .get("/session")
 .check(status().is(200));
+
+export const products = http("Products")
+  .get("/products?page=0&search=")
+  .check(status().is(200),
+  );
